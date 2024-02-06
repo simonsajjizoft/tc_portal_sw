36ece73afe3196eb3278941e87774298fb790c52
simonsajjizoft <simon.sajimathews@zoftsolutions.com>
2024-02-06 12:36:30 +0530

    Template build configs and ui changes

--- src/app/views/exercises/details/details.component.ts
+++ src/app/views/exercises/details/details.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { StepperOrientation } from '@angular/cdk/stepper';
-import { Component, OnInit } from '@angular/core';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import * as ClassicEditorBuild from 'ckeditor5-build-classic/build/ckeditor';
@@ -25,14 +26,28 @@ export class DetailsComponent {
   stepperOrientation: Observable<StepperOrientation>;
   public Editor = ClassicEditorBuild;
   statusDropdown;
+  content;
+  descenabled = true;
+  @ViewChild('desc') desc:ElementRef;
+  @ViewChild('autosize') autosize: CdkTextareaAutosize;
+  private _ngZone: NgZone;
   constructor( 
     private _formBuilder: FormBuilder, 
     breakpointObserver: BreakpointObserver,
     private dialog:MatDialog
-    ) { }
+    ) { 
+      this.firstFormGroup.valueChanges.subscribe(value => {
+        this.desc.nativeElement.style.height = 'auto';
+        this.desc.nativeElement.style.height = `${this.desc.nativeElement.scrollHeight}px`;
+
+    })
+    }
 
   ngOnInit(): void {
+    this.content = 'foiqnfoqnjf oqnfoq fjo qfojn qojf q fqofnqo qpofqpmf pqijfpqmfkq fipq33nmfipq3nmfpiqnmfpin fqj foq fioqnfjq foiqnfqj foqnfjl qfj quiofnqofnqjl fqoi fqionfqio fqolnfoqn folqn folqnfoj qfq '
+    this.firstFormGroup.get("description").setValue(this.content);
   }
+
 
   goStepForward(stepper:any){
     stepper.next();
@@ -58,6 +73,11 @@ export class DetailsComponent {
       panelClass: 'custom-modalbox',
       data: { data:'data' }
     });
+  }
+
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this._ngZone.onStable.pipe().subscribe(() => this.autosize.resizeToFitContent(true));
   }
 
 }
