3c9123f3f00fe3cae491b5ac0aa88f942c074257
simonsajjizoft <simon.sajimathews@zoftsolutions.com>
2024-02-27 22:41:14 +0530

    Refresh token Integrated into app

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -38,9 +38,7 @@ export class AuthService {
     // Assuming your refresh token API returns a new access token
     // You should replace this with your actual refresh token API call
     let url = environment.apiUrl + 'refresh-token';
-    // let data = { token: localStorage.getItem('userToken') ? localStorage.getItem('userToken') : '' };
-    let data = {token:'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJzaW1vbi5zYWppbWF0aGV3c0B6b2Z0c29sdXRpb25zLmNvbSo2NWQ3MmJlNTYyNGE2YzRmNzVhOTExZWQiLCJhdWQiOiJzY2hvb2x3aXphcmQtdXNlcnMiLCJuYmYiOjE3MDkwNDUzMDUsImlzcyI6Imh0dHA6Ly93d3cuc2Nob29sd2l6YXJkLmNvbSIsIlJvbGVzIjpbIlRDIl0sImV4cCI6MTcwOTA1MzMxMiwiaWF0IjoxNzA5MDQ5NzEyLCJqdGkiOiIyMTkxYjY4MC1kYzZhLTQ3NTAtOGEwMi0yY2VkZmQyNTdlODYifQ.ExkwsP02Wy6sAEIkFj1BdF-vyjj-X7GEI7q98uYJZHJo49b_WTQsApVBKFWUhwWaWxD1bX8AvCLSLMNFv-rqrlBWa4nDmQuG-76yLICHacijOs_uWkKqKxGRoKJLQ3mcr913HsMC0f5-iCybfOiDfZq8pfLMVWbN9l-EiK0KXB893-tEwERNGQsKAkHL6l5QcrdhlRzR_9Z9cIcMVBf23LVYh__XncWlBzdWLofJIiMdbj7MIuHhJTZjpfkwpwVexGXDkqP1q971jjstmPjvJxObwyUhxmWIKUkLt6ONsSb9gmLZzA6ufixmY7VLBJBXfzonM6f_SJQUlvdXQjBlcw'};
-
+    let data = { token: localStorage.getItem('userToken') ? localStorage.getItem('userToken') : '' };
     return this.http.post<any>(url, data).pipe(
       map(response => {
         // Assuming the response contains the new access token
